5f906e7837e193a80eb0a4bdd746e80a7f7f125d
anatoliysh66 <anatoliysh66@gmail.com>
2024-06-21 16:57:02 +0300

    дополнение 0,0,2+2

--- bin/swords_and_magic.dart
+++ bin/swords_and_magic.dart
@@ -13,4 +13,15 @@ void main(List<String> arguments) {
   final menu = stdin.readLineSync();
   print('Вы выбрали $menu');
   print('Новая игра...');
+
+  /// Имя главного героя
+  print('Введите имя главного героя:');
+  final name = stdin.readLineSync();
+
+  /// Класс главного героя
+  print('Введите название класса: ');
+  final typeHero = stdin.readLineSync();
+  print('$name Добро в мир Меча и Магии');
+  print('Ваш класс $typeHero');
+  print('Игра началась...');
 }
