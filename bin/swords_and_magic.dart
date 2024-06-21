e99475410d1086858501572c079a336f238657c3
anatoliysh66 <anatoliysh66@gmail.com>
2024-06-21 17:22:21 +0300

    Выносим реализации в функции.

--- bin/swords_and_magic.dart
+++ bin/swords_and_magic.dart
@@ -2,39 +2,59 @@ import 'dart:io';
 
 void main(List<String> arguments) {
   /// Меню
+  showMenu();
+
+  print('Введите необходимый пункт: ');
+  // Приводим строку к типу int
+  final int menu = int.tryParse(stdin.readLineSync() ?? "0") ?? 0;
+
+  if (checkEnter(menu)) {
+    print('Вы ввели не правильный пункт меню!');
+  } else if (menu == 1) {
+    newGame();
+  } else if (menu == 2) {
+    loadGame();
+  } else if (menu == 3) {
+    exitGame();
+  }
+}
+
+/// Проверяем на правильность ввода пункта меню
+bool checkEnter(int menu) {
+  return menu < 0 || menu > 3;
+}
+
+/// Выход из игры
+void exitGame() {
+  print('Выходим из игры...');
+}
+
+/// Загрузка игры
+void loadGame() {
+  print('Загрузить игру...');
+}
+
+/// Новая игра
+void newGame() {
+  print('Новая игра...');
+
+  /// Имя главного героя
+  print('Введите имя главного героя:');
+  final name = stdin.readLineSync();
+
+  /// Класс главного героя
+  print('Введите название класса: ');
+  final typeHero = stdin.readLineSync();
+  print('$name Добро в мир Меча и Магии');
+  print('Ваш класс $typeHero');
+  print('Игра началась...');
+}
+
+/// Вывод меню
+void showMenu() {
   print('1.Новая игра');
   print('2.Загрузить игру');
   print('3.Выйти');
 
   print('\n');
-
-  print('Введите необходимый пункт: ');
-  final int menu = int.tryParse(stdin.readLineSync() ?? "0") ?? 0;
-// Проверяем на правильность ввода пункта меню и выводим сообщение
-  if (menu <= 0 || menu > 3) {
-    print('Вы ввели не правильный пункт меню!');
-  } else if (menu == 1) {
-    // Новая игра
-
-    print('Новая игра...');
-
-    /// Имя главного героя
-    print('Введите имя главного героя:');
-    final name = stdin.readLineSync();
-
-    /// Класс главного героя
-    print('Введите название класса: ');
-    final typeHero = stdin.readLineSync();
-    print('$name Добро в мир Меча и Магии');
-    print('Ваш класс $typeHero');
-    print('Игра началась...');
-  } else if (menu == 2) {
-    // Если пользователь ввел пункт меню 2, то выводим сообщение о том,
-    // что игра загружается
-    print('Загрузить игру...');
-  } else if (menu == 3) {
-    // Если сли пользователь ввел пункт меню 3,
-    // то выводим сообщение о том, что выходим из игры
-    print('Выходим из игры...');
-  }
 }
