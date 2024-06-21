794d147f38c509b1b3319a83645bb2ef53057300
anatoliysh66 <anatoliysh66@gmail.com>
2024-06-21 18:08:44 +0300

    ошибки 13

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,30 @@
-import 'package:swords_and_magic/swords_and_magic.dart';
+import 'dart:io';
 
-void main(List<String> arguments) {
-  startGame();
+import 'package:swords_and_magic/features/game_over.dart';
+import 'package:swords_and_magic/features/player/player_consts.dart';
+import 'package:swords_and_magic/features/player/player_entity.dart';
+
+/// Новая игра
+void newGame() {
+  print('Новая игра...');
+
+  /// Имя главного героя
+  print('Введите имя главного героя:');
+  final name = stdin.readLineSync();
+
+  /// Класс главного героя
+  print('''
+Выберите класс игрока, введите число от 1 до 3: 
+1.Воин
+2.Паладин
+3.Маг
+''');
+  final typeHero = stdin.readLineSync();
+
+  initPlayer(inPlayerName: name ?? '', inPlayerClass: typeHero ?? '');
+
+  print('${playerEntity[playerName]} Добро в мир Меча и Магии');
+  print('Ваш класс ${playerEntity[playerClass]}');
+  print('Игра началась...');
+  exitGame();
 }
--- test/swords_and_magic_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:swords_and_magic/swords_and_magic.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
